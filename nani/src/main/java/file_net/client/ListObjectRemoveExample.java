50202ec070887f9c8f3907743cc49e97b7c60c54
ppsh7089@gmail.com <ppsh7089@gmail.com>
2020-05-31 17:50:52 +0900

    ㅁㄴㅇㄹ

--- nani/src/main/java/file_net/client/ListObjectRemoveExample.java
+++ nani/src/main/java/file_net/client/ListObjectRemoveExample.java
@@ -8,33 +8,16 @@ import java.util.List;
  
 
 public class ListObjectRemoveExample {
-
-	
-
 	public static void main(String[] args) {
-
 		HashMap<String, Integer> map = new HashMap<String, Integer>();	
-
-		
-
 //		if(!map.containsKey("서울")) {
-
 //			map.put("서울", 1);
-
 //		}else {
-
 //			map.put("서울", map.get("서울") + 1); 
-
-//		}
-
-//		
-
+//		}		
 //		String[] sList = {"서울", "광주", "서울"};
-
 //		서울, 1
-
 //		광주, 1
-
 //		서울, 2
 
 		
@@ -44,13 +27,9 @@ public class ListObjectRemoveExample {
 		List<WordCloud> list = new ArrayList<WordCloud>();
 
 		WordCloud w1 = new WordCloud("코딩", 1);
-
 		list.add(w1);
-
 		WordCloud w2 = new WordCloud("코딩", 2);
-
 		list.remove(w2);
-
 		list.add(w2);
 
 		
@@ -58,9 +37,7 @@ public class ListObjectRemoveExample {
 		
 
 		System.out.println(w1.equals(w2));
-
 		System.out.println(list.size());
-
 		System.out.println(list.get(0).getSize());
 
 	}
