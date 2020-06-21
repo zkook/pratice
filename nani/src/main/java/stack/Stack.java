9674d0251f660a528284c0507672945c86342fa0
ppsh7089@gmail.com <ppsh7089@gmail.com>
2020-06-21 18:42:15 +0900

    h

---
+++ nani/src/main/java/stack/Stack.java
@@ -0,0 +1,21 @@
+package stack; 
+
+/**
+ * <pre>
+ * stack 
+ * Stack.java
+ *
+ * 설명 :
+ * </pre>
+ * 
+ * @since : 2020. 6. 21.
+ * @author : zkook
+ * @version : v1.0
+ */
+public class Stack {
+	private final int MAX_SIZE = 50;
+	private int top = -1;
+	private int stack[];
+    
+
+}
