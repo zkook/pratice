e0f67cf8c6132bae443ed0702abeac811458ed24
ppsh7089@gmail.com <ppsh7089@gmail.com>
2020-07-19 16:10:26 +0900

    kdsvkvdbdk

--- nani/src/main/java/naver_api/Article.java
+++ nani/src/main/java/naver_api/Article.java
@@ -1,6 +1,33 @@
 package naver_api;
+
+/**
+ * <pre>
+ * naver_api 
+ * Article.java
+ *
+ * 설명 :
+ * </pre>
+ * 
+ * @since : 2020. 7. 19.
+ * @author : msi-user
+ * @version : v1.0
+ */
 public class Article 
 {
-	String id;
-	String content;
+	private String id;
+	private String content;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
+
